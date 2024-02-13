94c5b6db5c60c0150a6d94f383335760edf61512
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-13 21:21:16 +0100

    Enhance layout responsiveness for diverse screen sizes in Trending component

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -17,15 +17,19 @@ const Trending = () => {
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {trending.map((coin) => (
           <div className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300">
-            <div>
-              <div>
-                <img src={coin.item.small} alt="/" />
+            <div className="flex w-full items-center justify-between">
+              <div className="flex">
+                <img
+                  className="mr-4 rounded-full"
+                  src={coin.item.small}
+                  alt="/"
+                />
                 <div>
-                  <p>{coin.item.name}</p>
+                  <p className="font-bold">{coin.item.name}</p>
                   <p>{coin.item.symbol}</p>
                 </div>
               </div>
-              <div>
+              <div className="flex items-center">
                 <img
                   className="w-4 mr-2"
                   src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
