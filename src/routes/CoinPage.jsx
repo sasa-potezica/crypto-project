99625eaec06571ac0f0471a02433d8f7d813ec19
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-13 21:37:29 +0100

    Handle API response and update state in CoinPage component

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CoinPage = () => {
+  const [coin, setCoin] = useState({});
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data);
+    });
+  }, [url]);
   return <div>CoinPage</div>;
 };
 
