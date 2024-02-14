4e676c6a6b100c500e71c841ddd3217bffd5b826
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-14 16:04:08 +0100

    Implement loading state while fetching data in CoinPage component

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinPage = () => {
   const [coin, setCoin] = useState({});
@@ -11,7 +12,56 @@ const CoinPage = () => {
       setCoin(response.data);
     });
   }, [url]);
-  return <div>CoinPage</div>;
+  return (
+    <div>
+      <div>
+        <img src={coin.image?.large} alt="/" />
+        <div>
+          <p>{coin?.name} price</p>
+          <p>({coin.symbol?.toUpperCase()} / USD)</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            {coin.market_data?.current_price ? (
+              <p>${coin.market_data.current_price.usd.toLocaleString()}</p>
+            ) : null}
+            <p>7 Day</p>
+          </div>
+          <div>
+            <Sparklines data={coin.market_data?.sparkline_7d.price}>
+              <SparklinesLine color="teal" />
+            </Sparklines>
+          </div>
+          <div>
+            <div>
+              <p>Market Cap</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.market_cap.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+            <div>
+              <p>Volume 24h</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.total_volume.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>24h High</p>
+              {coin.market_data?.high_24h ? (
+                <p>${coin.market_data.high_24h.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoinPage;
