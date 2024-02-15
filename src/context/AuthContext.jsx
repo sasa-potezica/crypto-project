92be4d542821713c307dbeb3a602a2e518aebb69
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-15 13:39:29 +0100

    Create AuthContextProvider component for managing authentication context and setup Firebase configuration

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { auth, db } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+const UserContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+    return setDoc(doc(db, "users", email), {
+      watchList: [],
+    });
+  };
+};
