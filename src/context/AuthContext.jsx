a768cd839a29046b127c0de781c9f76945965b90
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-15 15:17:56 +0100

    Create AuthContextProvider and Implement SignUp component for user registration

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,10 +11,35 @@ import { doc, setDoc } from "firebase/firestore";
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
   const signUp = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password);
     return setDoc(doc(db, "users", email), {
       watchList: [],
     });
   };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ signUp, signIn, logOut, user }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(UserContext);
 };
