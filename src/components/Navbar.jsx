02463c0e3fcad285a82833dc911399e8553165b7
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-15 13:01:39 +0100

    Add SignIn component for user authentication and Add SignUp component for user registration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,11 @@ const Navbar = () => {
         <ThemeToggle />
       </div>
       <div className="hidden md:block">
-        <Link to="/singin" className="p-4 hover:text-accent">
+        <Link to="/signin" className="p-4 hover:text-accent">
           Sign In
         </Link>
         <Link
-          to="/singup"
+          to="/signup"
           className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
         >
           Sign Up
