f699f095070217ef9bf4a6f38d48da241f864288
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-15 13:16:35 +0100

    Implement SavedCoin component for displaying saved coins

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import SavedCoin from "../components/SavedCoin";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Account</h1>
+          <div>
+            <p>Welcome, User</p>
+          </div>
+        </div>
+        <div>
+          <button>Sign out</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>Watch List</h1>
+          <SavedCoin />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
